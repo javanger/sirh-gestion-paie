c5482465b5469f4be0437e94a4ab9364ef328844
Emmanuel Chêne <emma.chene@gmail.com>
2018-03-30 15:32:06 +0200

    TP02 - Mise en place

---
+++ src/main/java/dev/paie/config/ServicesConfig.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package dev.paie.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Emmanuel
+ *
+ */
+@Configuration
+@ComponentScan("dev.paie.service")
+public class ServicesConfig {
+	
+	
+	
+}
---
+++ src/main/java/dev/paie/service/CalculerRemunerationServiceSimple.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package dev.paie.service;
+
+import org.springframework.stereotype.Service;
+
+import dev.paie.entite.BulletinSalaire;
+import dev.paie.entite.ResultatCalculRemuneration;
+
+/**
+ * @author Emmanuel
+ *
+ */
+@Service
+public class CalculerRemunerationServiceSimple implements CalculerRemunerationService
+{
+
+	/* (non-Javadoc)
+	 * @see dev.paie.service.CalculerRemunerationService#calculer(dev.paie.entite.BulletinSalaire)
+	 */
+	@Override
+	public ResultatCalculRemuneration calculer(BulletinSalaire bulletin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
