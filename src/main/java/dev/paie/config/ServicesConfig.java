fadf80484b55418c0ca00d2339ceb338e5fb4c79
Kevin M <>
2018-03-30 15:48:00 +0200

    prise en main de spring test

---
+++ src/main/java/dev/paie/config/ServicesConfig.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package dev.paie.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Kevin M.
+ *
+ */
+@Configuration
+@ComponentScan("dev.paie.service")
+public class ServicesConfig {
+
+}
