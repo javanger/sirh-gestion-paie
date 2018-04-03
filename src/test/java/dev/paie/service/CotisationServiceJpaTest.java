68e52235a04a2fb17ee4d6ddf21d0a6b52902dcf
Axel Buteau <buteauaxel@gmail.com>
2018-04-03 17:34:46 +0200

    correction testjpa

--- src/test/java/dev/paie/service/CotisationServiceJpaTest.java
+++ src/test/java/dev/paie/service/CotisationServiceJpaTest.java
@@ -45,35 +45,35 @@ public class CotisationServiceJpaTest {
 
 		cotisationService.sauvegarder(cot);
 
-		// TODO vérifier qu'il est possible de récupérer la nouvelle cotisation
+		//  vérifier qu'il est possible de récupérer la nouvelle cotisation
 		// via la méthode lister
 		for (Cotisation c : cotisations) {
 			if (c.getCode().equals("test")) {
 				assertThat(c.getCode(), equalTo("test"));
-				assert new BigDecimal("45").compareTo(c.getTauxPatronal()) == 0;
-				assert new BigDecimal("50.2").compareTo(c.getTauxSalarial()) == 0;
+				assert new BigDecimal("8").compareTo(c.getTauxPatronal()) == 0;
+				assert new BigDecimal("9").compareTo(c.getTauxSalarial()) == 0;
 			}
 		}
 
-		// TODO modifier une cotisation
+		//  modifier une cotisation
 		cot.setCode("test");
 		cot.setLibelle("libelleTest");
 		cot.setTauxPatronal(new BigDecimal("0.8"));
 		cot.setTauxSalarial(new BigDecimal("1.2"));
 		cotisationService.mettreAJour(cot);
 
-		// TODO vérifier que les modifications sont bien prises en compte via la
+		//  vérifier que les modifications sont bien prises en compte via la
 		// méthode lister
 		listCotisation = cotisationService.lister();
 		for (Cotisation c : listCotisation) {
-			if (c.getCode().equals("tester")) {
-				assertThat(c.getCode(), equalTo("tester"));
-				assert new BigDecimal("0.7").compareTo(c.getTauxPatronal()) == 0;
-				assert new BigDecimal("1.1").compareTo(c.getTauxSalarial()) == 0;
-				cotisationService.supprimer(cot);
+			if (c.getCode().equals("test")) {
+				assertThat(c.getLibelle(), equalTo("libelleTest"));
+				assert new BigDecimal("0.8").compareTo(c.getTauxPatronal()) == 0;
+				assert new BigDecimal("1.2").compareTo(c.getTauxSalarial()) == 0;
+				
 			}
 		}
-		
+		cotisationService.supprimer(cot);
 		 listCotisation = cotisationService.lister();
 		 assertThat(listCotisation.size(), equalTo(0));
 	}
