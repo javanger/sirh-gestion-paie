6942002f78ebe747ff321c4b23a403435b1cd2e5
ange <abdjinan@yahoo.fr>
2018-04-03 15:49:13 +0200

    tp spring jdbc

--- src/main/java/dev/paie/config/DataSourceH2Config.java
+++ src/main/java/dev/paie/config/DataSourceH2Config.java
@@ -5,16 +5,16 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
 public class DataSourceH2Config {
 	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
-		dataSource.setUrl("jdbc:mariadb://localhost:3306/sirh_paie");
-		dataSource.setUsername("root");
-		dataSource.setPassword("");
-		return dataSource;
+	public DataSource datasource() {
+	return new EmbeddedDatabaseBuilder()
+	.setType(EmbeddedDatabaseType.H2)
+	.addScript("schema.sql")
+	.build();
 	}
 }
