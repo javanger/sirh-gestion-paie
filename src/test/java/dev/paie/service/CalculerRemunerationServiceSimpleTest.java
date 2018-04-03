c9cea2160aa96865430a771e504965d8a9111686
Kevin M <>
2018-04-03 15:20:33 +0200

    Importer que les services specifiques dans le fichier CalculerRemunerationServiceSimpleTest

--- src/test/java/dev/paie/service/CalculerRemunerationServiceSimpleTest.java
+++ src/test/java/dev/paie/service/CalculerRemunerationServiceSimpleTest.java
@@ -10,12 +10,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import dev.paie.config.JeuxDeDonneesConfig;
-import dev.paie.config.ServicesConfig;
 import dev.paie.entite.BulletinSalaire;
 import dev.paie.entite.ResultatCalculRemuneration;
+import dev.paie.util.PaieUtils;
 
 // Sélection des classes de configuration Spring à utiliser lors du test
-@ContextConfiguration(classes = { ServicesConfig.class, JeuxDeDonneesConfig.class })
+@ContextConfiguration(classes = { CalculerRemunerationServiceSimple.class, PaieUtils.class, JeuxDeDonneesConfig.class })
 
 // Configuration JUnit pour que Spring prenne la main sur le cycle de vie du
 @RunWith(SpringRunner.class)
@@ -24,28 +24,30 @@ public class CalculerRemunerationServiceSimpleTest {
 
 	@Autowired
 	private CalculerRemunerationService remunerationService;
-	
-	@Autowired BulletinSalaire bulletin1;
+
+	@Autowired
+	BulletinSalaire bulletin1;
 
 	@Test
 	public void test_not_null_remunerationService() {
 		assert null != remunerationService;
 	}
-	
+
 	@Test
 	public void test_not_null_bulletin1() {
 		assert null != bulletin1;
 	}
-	
+
 	@Test
 	public void test_calculer() {
 		// TODO remplacer null par un objet bulletin
 		ResultatCalculRemuneration resultat = remunerationService.calculer(bulletin1);
 		assertThat(resultat.getSalaireBrut(), equalTo("2683.30"));
-//		assertThat(resultat.getTotalRetenueSalarial(), equalTo("517.08"));
-//		assertThat(resultat.getTotalCotisationsPatronales(), equalTo("1096.13"));
-//		assertThat(resultat.getNetImposable(), equalTo("2166.22"));
-//		assertThat(resultat.getNetAPayer(), equalTo("2088.41"));
+		// assertThat(resultat.getTotalRetenueSalarial(), equalTo("517.08"));
+		// assertThat(resultat.getTotalCotisationsPatronales(),
+		// equalTo("1096.13"));
+		// assertThat(resultat.getNetImposable(), equalTo("2166.22"));
+		// assertThat(resultat.getNetAPayer(), equalTo("2088.41"));
 	}
 
 }
--- src/test/java/dev/paie/service/GradeServiceJdbcTemplateTest.java
+++ src/test/java/dev/paie/service/GradeServiceJdbcTemplateTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import dev.paie.config.DataSourceH2Config;
-// import dev.paie.config.DataSourceMariadbConfig;
 import dev.paie.config.ServicesConfig;
 import dev.paie.entite.Grade;
 
