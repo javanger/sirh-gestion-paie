2f63b79a7135e0608e6c36e08cd5184d78a72257
Axel Buteau <buteauaxel@gmail.com>
2018-04-03 10:15:36 +0200

    tp02-config-java

---
+++ src/main/java/dev/paie/config/ServicesConfig.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package dev.paie.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Axel B.
+ *
+ */
+@Configuration
+@ComponentScan("dev.paie.service")
+public class ServicesConfig {
+
+}
