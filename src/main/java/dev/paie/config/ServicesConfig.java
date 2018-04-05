545ec8f5964e2d49f9990ce5b6c5a504278ed5c6
Kevin M <>
2018-04-05 12:05:59 +0200

    correction import du datasource

--- src/main/java/dev/paie/config/ServicesConfig.java
+++ src/main/java/dev/paie/config/ServicesConfig.java
@@ -14,7 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @Configuration
 @ComponentScan({ "dev.paie.services", "dev.paie.utils" })
-@Import({ JpaConfig.class, JeuxDeDonneesConfig.class, DataSourceMariadbConfig.class })
+@Import({ JpaConfig.class, JeuxDeDonneesConfig.class, DataSourceH2Config.class })
 @EnableJpaRepositories("dev.paie.repositories")
 public class ServicesConfig {
 
